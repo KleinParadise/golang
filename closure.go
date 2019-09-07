03ad49bd841146da061c631f938f99b2d4a39511
KleinParadise <43290315+KleinParadise@users.noreply.github.com>
2019-09-07 21:39:40 +0800

    Update and rename Test.go to  closure.go

--- closure.go
+++ closure.go
@@ -1,8 +1,7 @@
-```lua
+
 package main
-
 import "fmt"
-
+//闭包
 func adder() func(int) int {
 	sum := 0
 	return func(x int) int {
@@ -10,7 +9,7 @@ func adder() func(int) int {
 		return sum
 	}
 }
-
+//adder()函数返回一个参数为int并且返回为int的匿名函数
 func main() {
 	pos, neg := adder(), adder()
 	for i := 0; i < 10; i++ {
@@ -20,4 +19,5 @@ func main() {
 		)
 	}
 }
-```
+//pos,neg等于adder()return的func(x,int) int函数。通过该main函数实现对adder()函数的sum变量闭包操作。
+
